6c521a2baf0dac90eea4e6f87078a51baa102ba2
Jesse A. Tov <jesse.tov@gmail.com>
2018-05-16 02:13:07 -0500

    (cargo-release) generate docs

---
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["bv"] = [{text:"impl&lt;'a, Block:&nbsp;<a class=\"trait\" href=\"bv/trait.BlockType.html\" title=\"trait bv::BlockType\">BlockType</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"bv/struct.BitSlice.html\" title=\"struct bv::BitSlice\">BitSlice</a>&lt;'a, Block&gt;",synthetic:false,types:["bv::slice::BitSlice"]},{text:"impl&lt;'a, Block:&nbsp;<a class=\"trait\" href=\"bv/trait.BlockType.html\" title=\"trait bv::BlockType\">BlockType</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"bv/struct.BitSliceMut.html\" title=\"struct bv::BitSliceMut\">BitSliceMut</a>&lt;'a, Block&gt;",synthetic:false,types:["bv::slice::BitSliceMut"]},{text:"impl&lt;Block:&nbsp;<a class=\"trait\" href=\"bv/trait.BlockType.html\" title=\"trait bv::BlockType\">BlockType</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"bv/struct.BitVec.html\" title=\"struct bv::BitVec\">BitVec</a>&lt;Block&gt;",synthetic:false,types:["bv::bit_vec::BitVec"]},];
+implementors["syn"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"syn/struct.Ident.html\" title=\"struct syn::Ident\">Ident</a>",synthetic:false,types:["syn::ident::Ident"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"syn/struct.Lifetime.html\" title=\"struct syn::Lifetime\">Lifetime</a>",synthetic:false,types:["syn::lifetime::Lifetime"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
